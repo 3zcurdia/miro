0f8c8fdcd2c7b9aba771c212c4dfa0b7da126b10
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2022-08-08 17:54:01 -0500

    Set intput on test files

--- spec/miro/downsampler/historgam_spec.rb
+++ spec/miro/downsampler/historgam_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe Miro::Downsampler::Histogram do
   let(:filepath) { File.join(__dir__, "../../fixtures/test.png") }
   let(:tempfile) { File.open(filepath) }
   let(:subject) { described_class.new(tempfile) }
+  let(:histogram_output) { File.read(File.join(__dir__, "../../fixtures/histogram.txt")) }
+
+  before do
+    allow(subject).to receive(:run_convert_command).and_return(histogram_output)
+  end
+
   it_behaves_like "downsampler"
 end
